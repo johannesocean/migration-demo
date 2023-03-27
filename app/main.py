8d27c33285bf56e9cdd333543c88bd738273bcb2
Johannes Johansson <johannes.johansson@queenslab.se>
2023-03-27 11:33:48 +0200

    minor

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@
 Created on 2022-12-08 15:19
 @author: johannes
 """
+import uvicorn
 from fastapi import FastAPI
 from app.routes import api_router
 
@@ -10,11 +11,10 @@ app.include_router(api_router)
 
 
 if __name__ == '__main__':
-    import uvicorn
     uvicorn.run(
-        'main:app',
+        app,
         host='0.0.0.0',
         port=8000,
         log_level='info',
-        reload=True
+        reload=False
     )
