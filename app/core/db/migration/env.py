e67ba25e4fcac086b34c36ab7a67e507eafe6288
Johannes Johansson <johannes.johansson@queenslab.se>
2023-09-06 13:17:02 +0200

    fix: update engine + compose

--- app/core/db/migration/env.py
+++ app/core/db/migration/env.py
@@ -1,6 +1,7 @@
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
+from sqlalchemy import create_engine
 from sqlalchemy import pool
 
 from alembic import context
@@ -65,11 +66,12 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    # connectable = engine_from_config(
+    #     config.get_section(config.config_ini_section),
+    #     prefix="sqlalchemy.",
+    #     poolclass=pool.NullPool,
+    # )
+    connectable = create_engine(settings.DB_CONNECTION_STRING)
 
     with connectable.connect() as connection:
         context.configure(
